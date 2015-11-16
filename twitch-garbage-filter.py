2e1192d21d7ba1a3cc5d7e63d0c096dad1f62061
ctd1500 <ctd1500@gmail.com>
2015-11-15 22:52:54 -0800

    Adding extra filtered keywords
    filtering HOSTTARGET as it needlessly prints garbage
    replacing CLEARCHAT with a useful message
    
    From the implementation in PR #1

--- twitch-garbage-filter.py
+++ twitch-garbage-filter.py
@@ -1,14 +1,18 @@
 __module_name__ = 'Twitch Garbage Filter'
-__module_version__ = '0.1'
+__module_version__ = '0.2'
 __module_description__ = 'Filters some garbage messages'
 
 import hexchat
 
+filter = ["USERSTATE", "ROOMSTATE", "CLEARCHAT", "HOSTTARGET"]
+
 def rawgarbage_cb(word, word_eol, userdata):
-    if word[1] == "USERSTATE" or word[1] == "ROOMSTATE":
+    if word[1] in filter:
         # print("RAW: {}".format(word))
+        if word[1] == "CLEARCHAT":
+            print("\00304Chat was cleared by a moderator\003")
         return hexchat.EAT_HEXCHAT
 
 hexchat.hook_server("RAW LINE", rawgarbage_cb)
 
-print("\00304{} successfully loaded.\003".format(__module_name__))
+print("\00304{} v{} successfully loaded.\003".format(__module_name__, __module_version__))
